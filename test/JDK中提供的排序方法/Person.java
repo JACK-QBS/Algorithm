efcdc68d9f8c949abbac3a43dca45d19e8bafa91
JACK-QBS <2200838299@qq.com>
2021-04-11 17:34:36 +0800

    Person

---
+++ test/JDK中提供的排序方法/Person.java
@@ -0,0 +1,50 @@
+package useJDK;
+
+public class Person {
+    String name;
+    int age;
+    int height;
+    int weight;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
